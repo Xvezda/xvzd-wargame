8be968ba08bcee4e11024dd19a3eabbbe6d42388
Xvezda <xvezda@naver.com>
2019-07-29 01:07:35 +0900

    Modify main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_minify import minify
+from flaskext.mysql import MySQL
+
 app = Flask(__name__)
+#minify(app=app)
+
+mysql = MySQL()
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'mysql'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
 
 @app.route("/")
-def hello():
-  return "Hello World from Flask"
+def main():
+  return render_template('skeleton.html')
+  #return "Hello World from Flask"
 
 if __name__ == "__main__":
   # Only for debugging while developing
