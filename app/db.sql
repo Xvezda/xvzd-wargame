3fd03fda3aad31082212de309eaad801215487b4
Xvezda <xvezda@naver.com>
2019-07-29 01:29:47 +0900

    Modify db.sql

--- app/db.sql
+++ app/db.sql
@@ -1,9 +1,9 @@
-create user 'xvzd'@'localhost' identified by 'xvzd';
-grant all privileges on xvzd_wargame.* to 'xvzd'@'localhost';
-create database xvzd_wargame;
-create table `xvzd_wargame`.`xvzd_users` (
-	`uid` bigint not null auto_increment,
-	`id` varchar(128) not null,
-	`name` varchar(128) not null,
-	`password` varchar(128) not null,
+create user if not exists 'xvzd'@'%' identified by 'xvzd';
+grant all privileges on xvzd_wargame.* to 'xvzd'@'%';
+create database if not exists xvzd_wargame;
+create table if not exists `xvzd_wargame`.`xvzd_users` (
+    `uid` bigint not null auto_increment,
+    `id` varchar(128) not null,
+    `name` varchar(128) not null,
+    `password` varchar(128) not null,
 primary key (`uid`));
