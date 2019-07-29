69e27152deffad61c32317e689620b4a454b19d6
Xvezda <xvezda@naver.com>
2019-07-30 00:42:23 +0900

    Implement login

--- app/db.sql
+++ app/db.sql
@@ -1,9 +1,38 @@
+declare @adminUid bigint;
+set @adminUid = 1;
+
+/* Create user */
 create user if not exists 'xvzd'@'%' identified by 'xvzd';
-grant all privileges on xvzd_wargame.* to 'xvzd'@'%';
-create database if not exists xvzd_wargame;
+grant all privileges on `xvzd_wargame`.* to 'xvzd'@'%';
+
+/* Create database */
+create database if not exists `xvzd_wargame`
+character set utf8mb4 collate utf8mb4_unicode_ci;
+
 create table if not exists `xvzd_wargame`.`xvzd_users` (
     `uid` bigint not null auto_increment,
     `id` varchar(128) not null,
     `name` varchar(128) not null,
     `password` varchar(128) not null,
-primary key (`uid`));
+    primary key (`uid`),
+    unique (`id`)
+) ENGINE=InnoDB;
+create table if not exists `xvzd_wargame`.`xvzd_notice` (
+    `no` bigint not null auto_increment,
+    `title` varchar(128) not null,
+    `content` text not null,
+    `uid` bigint not null,
+    primary key (`no`)
+) ENGINE=InnoDB;
+
+/* Insert datas */
+insert into `xvzd_wargame`.`xvzd_users` (id, name, password)
+select * from (
+    select 'admin',
+           'Xvezda',
+           'fa449eec0fcabe9829db83e3810798f3f5e7b832ba79682cd4e474ddd51f5677b'
+           '42a556cb8530d509f970b765e88b9052d77192d520acbf562178fd5aeb808ad'
+) as tmp where not exists (
+    select `id` from `xvzd_wargame`.`xvzd_users` where id='admin'
+) limit 1;
+
--- app/main.py
+++ app/main.py
@@ -1,15 +1,25 @@
+#-*- coding: utf-8 -*-
 # Copyright (C) 2019 Xvezda <https://xvezda.com/>
 
+
+import re
 import os
+import hashlib
 
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import session
+from flask import abort
+from flask import render_template, request, redirect, escape
 from flask import send_from_directory
+from flask import make_response
 from flask_minify import minify
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 #minify(app=app)
+
+# NOTE: This is NOT a flag! >:)
+app.secret_key = os.getenv('SECRET_KEY', os.urandom(32))
 
 mysql = MySQL()
 # MySQL configurations
@@ -22,13 +32,13 @@ mysql.init_app(app)
 conn = mysql.connect()
 cursor = conn.cursor()
 
-hack = [
-  '.', '..', '#', '--', ':', '\\', '//', '`',  '$',
-  '_', '*', '|', '\n', '\r', '\b', '\0', 'php', 'system',
-  'union', 'collation', 'proc',
-]
 pages = ['notice', 'items', 'support']
 
+
+def check_hack(target):
+  pattern = r'\.|\.\.|#|--|:|\\|//|`|\$|_|\*|\||' + \
+            r'union|collation|proc|php|system'
+  return bool(re.findall(pattern, target, re.I))
 
 @app.route('/favicon.ico')
 def favicon():
@@ -41,31 +51,146 @@ def favicon():
 def redir_home():
   return redirect('/', code=302)
 
+@app.route('/logout')
+def logout():
+  if session.get('is_logged'):
+    session.clear()
+  return redirect('/', code=302)
+
+@app.route('/login')
+def login():
+  context = {
+    'title': 'Login',
+    'current_page': 'login',
+    'pages': pages
+  }
+  return render_template('skeleton.html', **context)
+
+@app.route('/login-check', methods=['GET', 'POST'])
+def login_check():
+  if request.method == 'POST':
+    # TODO: Add login process
+    require = ['user_id', 'user_pw']
+    if not all(field in request.form for field in require):
+      return abort(400, 'Something is missing!')
+
+    user_id = request.form['user_id'].strip()
+    user_pw = request.form['user_pw'].strip()
+
+    if check_hack(user_id+user_pw):
+      return abort(400, '')
+
+    cursor.execute('select name, password from xvzd_users where id = %s',
+                   (user_id))
+
+    result = cursor.fetchone()
+    name = result[0] if result else None
+    password = result[1] if result else None
+
+    pw_hash = hashlib.sha512(user_pw.encode('utf-8')).hexdigest()
+    if password == pw_hash:
+      session['is_logged'] = True
+      session['user_id'] = user_id
+      session['user_name'] = name
+
+      resp = make_response(redirect('/', code=302))
+      if user_id == 'admin':
+        session['is_admin'] = True
+      if session.get('is_admin', False):
+        # If admin
+        # NOTE: Admin will set flag here
+        resp.set_cookie('flag',
+                        os.getenv('WARGAME_FLAG', 'NO_FLAG'), httponly=True)
+      else:
+        resp.set_cookie('flag', 'admin_has_flag_here', httponly=True)
+      return resp
+    else:
+      return abort(400, """
+        <script>alert('ID, PW not match!');history.back();</script>
+      """)
+  else:
+    return redirect('/', code=302)
+
+@app.route('/join')
+def join():
+  context = {
+    'title': 'Join',
+    'current_page': 'join',
+    'pages': pages
+  }
+  return render_template('skeleton.html', **context)
+
+@app.route('/join-check', methods=['GET', 'POST'])
+def join_check():
+  if request.method == 'POST':
+    require = ['user_id', 'user_name', 'user_pw']
+    if not all(field in request.form for field in require):
+      return abort(400, 'Something is missing!')
+
+    user_id = request.form['user_id'].strip()[:0x80]
+    user_name = request.form['user_name'].strip()[:0x80]
+    user_pw = request.form['user_pw'].strip()[:0x80]
+
+    user_pw = hashlib.sha512(user_pw.encode('utf-8')).hexdigest()
+
+    try:
+      cursor.execute('''
+        insert into xvzd_users (id, name, password) values (%s, %s, %s)
+      ''', (user_id, user_name, user_pw))
+    except:
+      return abort(400, """
+        <script>alert('ID Already exists!');history.back();</script>
+      """)
+    conn.commit()
+
+    return 'join', 200
+  else:
+    return redirect('/', code=302)
+
 @app.route('/')
 @app.route('/<page>')
 def main(page='home'):
-  if any(word in page.lower() for word in hack):
-    return 'No hack!', 400
+  if check_hack(page):
+    return abort(400, '')
   context = {
     'title': page.title(),
     'current_page': page,
     'pages': pages
   }
   return render_template('skeleton.html', **context)
-  #return "Hello World from Flask"
 
+@app.errorhandler(400)
+def not_found(error):
+  context = {
+    'title': 'Error 400',
+    'current_page': '400',
+    'pages': pages,
+    'content': error.description
+  }
+  return render_template('skeleton.html', **context), 400
 
 @app.errorhandler(404)
 def not_found(error):
-  print dir(error)
   context = {
-    'title': 'Test',
-    'current_page': 'error',
+    'title': 'Error 404',
+    'current_page': '404',
     'pages': pages
   }
   return render_template('skeleton.html', **context), 404
+
+@app.errorhandler(403)
+def not_allowed(error):
+  context = {
+    'title': 'Error 403',
+    'current_page': '403',
+    'pages': pages
+  }
+  return render_template('skeleton.html', **context), 403
 
 
 if __name__ == "__main__":
   # Only for debugging while developing
   app.run(host='0.0.0.0', debug=True, port=8080)
+  # Close mysql connection
+  cursor.close()
+  conn.close()
